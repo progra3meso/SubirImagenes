b7cae15bbba27e9f9bde4d5760a53afec9854464
Guillermo Ramirez <gramz@mesoamericana.edu.gt>
2023-05-24 22:42:28 -0600

    Subir imagenes

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +11,57 @@ namespace SubirImagenes
 {
     public partial class _Default : Page
     {
+        private void GuardarJson(List<Producto> productos)
+        {
+            string json = JsonConvert.SerializeObject(productos);
+            string archivo = Server.MapPath("Productos.json");
+            System.IO.File.WriteAllText(archivo, json);
+        }
+
+        private List<Producto> LeerJson()
+        {
+            List<Producto> productos = new List<Producto>();
+            string archivo = Server.MapPath("Productos.json");
+            StreamReader jsonStream = File.OpenText(archivo);
+            string json = jsonStream.ReadToEnd();
+            jsonStream.Close();
+            productos = JsonConvert.DeserializeObject<List<Producto>>(json);
+
+            return productos;
+
+        }   
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void ButtonSubir_Click(object sender, EventArgs e)
+        {
+            List<Producto> productos = new List<Producto>();
+            productos = LeerJson();
+
+            Producto producto = new Producto();            
+            producto.Descripcion = TextBoxDescripcion.Text;
+            string archivo = "~/imagenes/" + FileUploadImagen.FileName;
+            producto.Imagen = archivo;
+
+            string archivoOrigen = Path.GetFileName(FileUploadImagen.FileName);
+
+            try
+            {
+                FileUploadImagen.SaveAs(Server.MapPath("~/imagenes/") + archivoOrigen);
+                LabelEstado.Text = "Exitosamente Subido";
+            }
+            catch (Exception ex)
+            {
+                LabelEstado.Text = "No se pudo subir, se generó el siguiente error:  " + ex.Message;
+            }
+                       
+            productos.Add(producto);
+           
+
+            GuardarJson(productos);
 
         }
     }
---
+++ Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SubirImagenes
+{
+    public class Producto
+    {
+        string descripcion;
+        string imagen;
+
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public string Imagen { get => imagen; set => imagen = value; }
+    }
+}
